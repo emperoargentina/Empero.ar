82bfb7005da320e17fdd0152a837a4228a829685
juandominguez <juanigdominguez03@gmail.com>
2026-08-01 22:03:45 -0300

    arreglo

--- src/pages/admin/views/ProductForm.tsx
+++ src/pages/admin/views/ProductForm.tsx
@@ -21,7 +21,6 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Switch } from '@/components/ui/switch'
-import { Badge } from '@/components/ui/badge'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
